ce37a4610518536826636d537de717c03b8b4074
Gaetan Dubuc <gaet.dub1@gmail.com>
2024-09-29 18:00:45 +0200

    rework notebook

--- make.bat
+++
@@ -1,8 +0,0 @@
-poetry shell
-poetry install
-kaggle competitions download -c titanic
-mkdir input\titanic
-mkdir \kaggle\temp/
-move titanic.zip input\titanic
-tar -xf input\titanic\titanic.zip -C %~dp0input\titanic
-python scripts/split_data.py
